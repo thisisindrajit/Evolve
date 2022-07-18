6adc2a401c7569728b8f7e24b96f117382b70c5f
Indrajit <43838718+thisisindrajit@users.noreply.github.com>
2022-07-18 15:58:26 +0530

    changed svg icon

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -253,32 +253,22 @@ const Dashboard = (props) => {
           {/* STOCKS */}
           <div>
             <div className="flex items-center justify-between">
-              <div className="flex items-center justify-center gap-1">
+              <div className="flex items-center justify-center gap-1.5">
                 <div className="text-lg md:text-xl">
                   My <span className="font-bold">Stocks</span>
                 </div>
                 <svg
-                  viewBox="0 0 22 22"
-                  className="cursor-pointer mb-[0.5] md:mb-0 h-5 w-5"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
+                  height="20"
+                  width="20"
+                  viewBox="0 0 46 46"
+                  className="cursor-pointer"
                   data-tip="Only US stocks are supported as of now."
                   data-for="info"
+                  fill="white"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <path
-                    d="M12 10.75C12.4142 10.75 12.75 11.0858 12.75 11.5V16.5C12.75 16.9142 12.4142 17.25 12 17.25C11.5858 17.25 11.25 16.9142 11.25 16.5V11.5C11.25 11.0858 11.5858 10.75 12 10.75Z"
-                    fill="white"
-                  />
-                  <path
-                    d="M12 9C12.5523 9 13 8.55228 13 8C13 7.44772 12.5523 7 12 7C11.4477 7 11 7.44772 11 8C11 8.55228 11.4477 9 12 9Z"
-                    fill="white"
-                  />
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M3.25 12C3.25 7.16751 7.16751 3.25 12 3.25C16.8325 3.25 20.75 7.16751 20.75 12C20.75 16.8325 16.8325 20.75 12 20.75C7.16751 20.75 3.25 16.8325 3.25 12ZM12 4.75C7.99594 4.75 4.75 7.99594 4.75 12C4.75 16.0041 7.99594 19.25 12 19.25C16.0041 19.25 19.25 16.0041 19.25 12C19.25 7.99594 16.0041 4.75 12 4.75Z"
-                    fill="white"
-                  />
+                  <path d="M0 0h48v48h-48z" fill="none" />
+                  <path d="M24 4c-11.05 0-20 8.95-20 20s8.95 20 20 20 20-8.95 20-20-8.95-20-20-20zm2 30h-4v-12h4v12zm0-16h-4v-4h4v4z" />
                 </svg>
                 <ReactTooltip
                   id="info"
@@ -349,32 +339,22 @@ const Dashboard = (props) => {
 
           <div>
             <div className="flex items-center justify-between">
-              <div className="flex items-center justify-center gap-1">
+              <div className="flex items-center justify-center gap-1.5">
                 <div className="text-lg md:text-xl">
                   My <span className="font-bold">Crypto</span>
                 </div>
                 <svg
-                  viewBox="0 0 22 22"
-                  className="cursor-pointer mb-[0.5] md:mb-0 h-5 w-5"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
+                  height="20"
+                  width="20"
+                  viewBox="0 0 46 46"
+                  className="cursor-pointer"
                   data-tip="Only cryptos trading in US dollars are supported as of now."
                   data-for="info"
+                  fill="white"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <path
-                    d="M12 10.75C12.4142 10.75 12.75 11.0858 12.75 11.5V16.5C12.75 16.9142 12.4142 17.25 12 17.25C11.5858 17.25 11.25 16.9142 11.25 16.5V11.5C11.25 11.0858 11.5858 10.75 12 10.75Z"
-                    fill="white"
-                  />
-                  <path
-                    d="M12 9C12.5523 9 13 8.55228 13 8C13 7.44772 12.5523 7 12 7C11.4477 7 11 7.44772 11 8C11 8.55228 11.4477 9 12 9Z"
-                    fill="white"
-                  />
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M3.25 12C3.25 7.16751 7.16751 3.25 12 3.25C16.8325 3.25 20.75 7.16751 20.75 12C20.75 16.8325 16.8325 20.75 12 20.75C7.16751 20.75 3.25 16.8325 3.25 12ZM12 4.75C7.99594 4.75 4.75 7.99594 4.75 12C4.75 16.0041 7.99594 19.25 12 19.25C16.0041 19.25 19.25 16.0041 19.25 12C19.25 7.99594 16.0041 4.75 12 4.75Z"
-                    fill="white"
-                  />
+                  <path d="M0 0h48v48h-48z" fill="none" />
+                  <path d="M24 4c-11.05 0-20 8.95-20 20s8.95 20 20 20 20-8.95 20-20-8.95-20-20-20zm2 30h-4v-12h4v12zm0-16h-4v-4h4v4z" />
                 </svg>
                 <ReactTooltip
                   id="info"
