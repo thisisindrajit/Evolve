f2c7489d60519c2bed94691a2128844d78650b1c
Indrajit <43838718+thisisindrajit@users.noreply.github.com>
2022-07-18 14:55:31 +0530

    small change in info svg width height

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -258,10 +258,8 @@ const Dashboard = (props) => {
                   My <span className="font-bold">Stocks</span>
                 </div>
                 <svg
-                  width="22"
-                  height="22"
                   viewBox="0 0 22 22"
-                  className="cursor-pointer mb-[0.25] md:mb-0 h-5 w-5"
+                  className="cursor-pointer mb-[0.5] md:mb-0 h-5 w-5"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
                   data-tip="Only US stocks are supported as of now."
@@ -356,10 +354,8 @@ const Dashboard = (props) => {
                   My <span className="font-bold">Crypto</span>
                 </div>
                 <svg
-                  width="22"
-                  height="22"
                   viewBox="0 0 22 22"
-                  className="cursor-pointer mb-[0.25] md:mb-0 h-5 w-5"
+                  className="cursor-pointer mb-[0.5] md:mb-0 h-5 w-5"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
                   data-tip="Only cryptos trading in US dollars are supported as of now."
